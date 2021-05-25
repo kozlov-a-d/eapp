e1fd49563d70a680824bab48d49019ffd759fd2a
Andrey Kozlov <andreydmitrievich666@gmail.com>
2021-05-25 22:05:12 +0300

    fix url prod

--- utils/absolute-url.ts
+++ utils/absolute-url.ts
@@ -1,4 +1,4 @@
 export default function absoluteUrl(relativeUrl: string): string {
-    const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.VERCEL_URL;
+    const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'eapp-rn03w1znp-kozlov-a-d.vercel.app';
     return url + relativeUrl;
 }
